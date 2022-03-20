b6fbe9a4a443eca7dd6d4be430df684754e97063
Mikhail Kyrpa <michael.kirpa@gmail.com>
2022-03-20 21:56:45 +0200

    second lesson - config

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"restapi-lesson/internal/config"
 	"restapi-lesson/internal/user"
 	"restapi-lesson/pkg/logging"
 	"time"
@@ -14,18 +19,41 @@ func main() {
 	logger := logging.GetLogger()
 	logger.Info("create roter")
 	router := httprouter.New()
+
+	cfg := config.GetConfig()
+
 	logger.Info("register user handler")
 	handler := user.NewHandler(logger)
 	handler.Register(router)
-	start(router)
+	start(router, cfg)
 }
 
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	logger := logging.GetLogger()
 	logger.Info("start application")
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		panic(err)
+
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == "sock" {
+		logger.Info("detect app path")
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+
+		logger.Info("listen unix socket")
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Info("server is listening unix socket: %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+		logger.Info("server is listening port %s:%s", cfg.Listen.BindIP, cfg.Listen.Port)
+	}
+	if listenErr != nil {
+		logger.Fatal(listenErr)
 	}
 
 	server := &http.Server{
@@ -34,6 +62,5 @@ func start(router *httprouter.Router) {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	logger.Info("server is listening port 0.0.0.0:8080")
 	logger.Fatal(server.Serve(listener))
 }
---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"restapi-lesson/pkg/logging"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	IsDebug bool `yaml:"is_debug"`
+	Listen  struct {
+		Type   string `yaml:"type"`
+		BindIP string `yaml:"bind_ip"`
+		Port   string `yaml:"port"`
+	} `yaml:"listen"`
+}
+
+var instance *Config
+var once sync.Once
+
+func GetConfig() *Config {
+	once.Do(func() {
+		logger := logging.GetLogger()
+		logger.Info("read application configuration")
+		instance = &Config{}
+		if err := cleanenv.ReadConfig("..\\..\\config.yml", instance); err != nil {
+			help, _ := cleanenv.GetDescription(instance, nil)
+			logger.Info(help)
+			logger.Fatal(err)
+		}
+	})
+	return instance
+}
